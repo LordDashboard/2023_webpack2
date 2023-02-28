49d48758d6439c7dab8ddf604a9a80132a2b7699
Ward Venrooij <ward@data-ink.nl>
2023-02-28 21:12:52 +0100

    SASS loaders installed no errors

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,12 @@ import { checkForName } from './js/nameChecker.js'
 import { handleSubmit, checkUrlValidity } from './js/formHandler.js'
 import { test } from './js/simpleCheck.js'
 
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
 
 checkForName("test")
 test()
--- webpack.prod.js
+++ webpack.prod.js
@@ -19,6 +19,10 @@ module.exports = {
                   presets: ['@babel/preset-env']
                 }
               }
+            },
+            {
+              test: /\.scss$/,
+             use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
           ]
     },
